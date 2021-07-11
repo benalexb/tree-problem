adc4e15d9f694a7459fb87fc0489e95accafc363
Benjamin Barreto <benalexb@gmail.com>
2021-07-11 18:26:39 +0200

    Basic tree, fetching live data from local server

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Tree from 'react-d3-tree';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data: fetchedData } = await axios.get('http://localhost:5000/api/graph');
+        setData(fetchedData);
+      } catch (error) {
+        console.error(error);
+      }
+    })()
+  }, []);
+
   return (
-    <div className="App">
-      Hello world!
+    <div className="app">
+      {!!data && (
+        <div id="treeWrapper" className="treeWrapper">
+          <Tree
+            data={data}
+            collapsible={false}
+            pathFunc="step"
+            translate={{ x: 500, y: 500 }}
+            nodeSize={{ x: 300, y: 140 }}
+            onNodeClick={(node) => {
+              window.alert(node.data.description); // bbarreto_dev: tmp
+            }}
+          />
+        </div>
+      )}
     </div>
   );
 }
