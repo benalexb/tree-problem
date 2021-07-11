d03adfd5a98081fdd6e5f116d74ff2e7e7a01b07
Benjamin Barreto <benalexb@gmail.com>
2021-07-11 20:57:43 +0200

    Added a popover on node click

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,18 +1,19 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import { useEffect, useState, useRef } from 'react';
 import Tree from 'react-d3-tree';
 import axios from 'axios';
 import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
+import Popover from '@material-ui/core/Popover';
 
 function App() {
   const [data, setData] = useState(null);
   const [open, setOpen] = useState(false);
   const [node, setNode] = useState({});
+  const circle = useRef(null);
 
   useEffect(() => {
     (async () => {
@@ -25,6 +26,11 @@ function App() {
     })()
   }, []);
 
+  const handleClose = () => {
+    setOpen(false);
+    circle.current.classList.remove('selected-circle');
+  }
+
   return (
     <div className="app">
       {!!data && (
@@ -35,29 +41,30 @@ function App() {
             pathFunc="step"
             translate={{ x: 500, y: 500 }}
             nodeSize={{ x: 300, y: 140 }}
-            onNodeClick={({ data }) => {
+            onNodeClick={({ data }, { target }) => {
+              target.classList.add('selected-circle');
+              circle.current = target;
               setNode(data);
               setOpen(true);
             }}
           />
         </div>
       )}
-      <Dialog
+      <Popover
         open={open}
-        onClose={() => setOpen(false)}
+        anchorEl={circle.current}
+        onClose={handleClose}
+        anchorOrigin={{ vertical: 'center', horizontal: 'center' }}
+        transformOrigin={{ vertical: 'top', horizontal: 'left' }}
       >
         <DialogTitle>{node.name}</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            {node.description}
-          </DialogContentText>
+          <DialogContentText>{node.description}</DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => setOpen(false)}>
-            Close
-          </Button>
+          <Button onClick={handleClose}>Close</Button>
         </DialogActions>
-      </Dialog>
+      </Popover>
     </div>
   );
 }
